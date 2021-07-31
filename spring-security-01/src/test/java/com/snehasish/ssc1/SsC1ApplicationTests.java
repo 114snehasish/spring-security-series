674a4f85fa121cd682be5affedfbc70a6c4a4d91
Snehasish Chakraborty <114snehasish@gmail.com>
2021-07-31 21:12:14 +0530

    spring security chapter 1 Out of the box functionality

---
+++ spring-security-01/src/test/java/com/snehasish/ssc1/SsC1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snehasish.ssc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsC1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
