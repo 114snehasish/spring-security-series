afd9d93be002d6c8c8657e5ed88fc08255df898e
Snehasish Chakraborty <114snehasish@gmail.com>
2021-07-31 23:15:16 +0530

    spring security chapter 1 changing to BcryptPasswordEncoder

--- spring-security-01/src/main/java/com/snehasish/ssc1/config/SecurityConfig.java
+++ spring-security-01/src/main/java/com/snehasish/ssc1/config/SecurityConfig.java
@@ -4,22 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         var userDetailsService = new InMemoryUserDetailsManager();
-        var user1 = User.withUsername("testuser").password("12345").authorities("read", "write").build();
+        var user1 = User.withUsername("testuser").password(passwordEncoder.encode("12345")).authorities("read", "write").build();
         userDetailsService.createUser(user1);
         return userDetailsService;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
