4437384a02c6e8adf804ce401577d56935a95cf3
Snehasish Chakraborty <114snehasish@gmail.com>
2021-08-31 21:55:05 +0530

    spring security chapter 2 add bcrypt and postman collection

--- spring-security-02/src/main/java/com/snehasish/ssc2/config/SecurityConfig.java
+++ spring-security-02/src/main/java/com/snehasish/ssc2/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import com.snehasish.ssc2.service.SecurityUser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -15,14 +15,14 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
 
     @Bean
-    public UserDetailsService userDetailsService(UserRepository userRepository) {
+    public UserDetailsService userDetailsService(UserRepository userRepository, PasswordEncoder encoder) {
         var userDetailsManager = new DBUserDetailsService(userRepository);
-        userDetailsManager.createUser(new SecurityUser(User.builder().userName("jdoe").password("12345").build()));
+        userDetailsManager.createUser(new SecurityUser(User.builder().userName("jdoe").password(encoder.encode("12345")).build()));
         return userDetailsManager;
     }
 
