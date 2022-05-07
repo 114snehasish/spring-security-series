594731c28bbab2b4d6621ca500a0db262013630f
Snehasish Chakraborty <114snehasish@gmail.com>
2022-05-07 17:53:33 +0530

    change to OncePerRequestFilter

--- spring-security-04/src/main/java/com/snehasish/springsecurity04/filters/CustomAuthFilter.java
+++ spring-security-04/src/main/java/com/snehasish/springsecurity04/filters/CustomAuthFilter.java
@@ -11,38 +11,38 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
 @Slf4j
-public class CustomAuthFilter implements Filter {
+public class CustomAuthFilter extends OncePerRequestFilter {
     public static final String HEADER_USERNAME = "username";
     public static final String HEADER_PASSWORD = "password";
     private AuthenticationManager authenticationManager;
 
     @Override
-    public void doFilter(ServletRequest servletRequest,
-                         ServletResponse servletResponse,
-                         FilterChain filterChain) throws IOException, ServletException {
-        val username = getHeaderValue(servletRequest, HEADER_USERNAME);
-        val password = getHeaderValue(servletRequest, HEADER_PASSWORD);
+    public void doFilterInternal(HttpServletRequest request,
+                                 HttpServletResponse response,
+                                 FilterChain filterChain) throws IOException, ServletException {
+        val username = request.getHeader(HEADER_USERNAME);
+        val password = request.getHeader(HEADER_PASSWORD);
         try {
-            if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password))
+            if (!StringUtils.hasLength(username) || !StringUtils.hasLength(password))
                 throw new BadCredentialsException("Username or password cannot be empty");
             val authentication = new UsernamePasswordAuthenticationToken(username, password);
             val fullAuthentication = authenticationManager.authenticate(authentication);
             SecurityContextHolder.getContext().setAuthentication(fullAuthentication);
-            filterChain.doFilter(servletRequest, servletResponse);
+            filterChain.doFilter(request, response);
         } catch (AuthenticationException ex) {
             log.error("Exception occurred while authenticating: {}", ex.getMessage());
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         }
-    }
-
-    private String getHeaderValue(ServletRequest servletRequest, String headerName) {
-        return ((HttpServletRequest) servletRequest).getHeader(headerName);
     }
 
     @Autowired
